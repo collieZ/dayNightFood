71e2bd9db69f536d78f9d4e693b9554372a59fa0
hentaizhou <623196395@qq.com>
2018-09-02 16:25:58 +0800

    增加自动添加分组 后续待验证

--- routes/wechat.js
+++ routes/wechat.js
@@ -15,14 +15,17 @@ var config = { // 微信公众号配置信息
     encodingAESKey: ''
 };
 
-var Staff = ["oZ1891Z9gSJfAjfu9Eu7kJdYEwA8", "oZ1891bzhM_M8biCTUJatGwUj7sA",
-    "oZ1891R5kyrqBNtEDn00bYg3e77Y", "oZ1891at7yq9I4Z5vYP0XEdX1PzQ"
+// 煮饭阿姨的openid
+var cookerId = 'oZ1891Z9gSJfAjfu9Eu7kJdYEwA8';
+
+// 现有员工的集合
+var StaffNow = ["oZ1891bzhM_M8biCTUJatGwUj7sA","oZ1891R5kyrqBNtEDn00bYg3e77Y",
+            "oZ1891at7yq9I4Z5vYP0XEdX1PzQ"
 ];
 
-// var app = express();    // 实例化express框架
 var api = new weChatApi(config.appid, config.appsecret);
 
-router.use(express.query());    // ??后面了解
+router.use(express.query());   
 
 var dailyFoodMenu = new Array();    // 每日菜单
 var foods = [];     // 订餐统计 {name: "", numbers: 0}
@@ -30,11 +33,8 @@ var foods = [];     // 订餐统计 {name: "", numbers: 0}
 router.use('/', wechat(config, function (req, res, next) {
     console.log(req.weixin);
     var message = req.weixin;
-    if (message.Content == "哈哈") {
-        res.reply("你发送的是哈哈");
-    }
 
-    if(!message.Content) {
+    if(!message.Content) {  // 防止进入回调后内容为null
         return;
     } 
     else if(message.Content.split('=')[0] == "今日菜单") {   // 煮饭阿姨更新每日菜单
@@ -44,11 +44,10 @@ router.use('/', wechat(config, function (req, res, next) {
             console.log(dailyFoodMenu);
             updateMenu();   // 将菜单更新到统计数组
             var sendMsg = send2EveryOne().toString();  // 将格式化好的待发送菜单数据传给字符串变量
-
             res.reply("今日菜单已更新并发送");
-            // 群发给每个员工  有点问题哦
+            // 群发给每个员工  
             try{
-                api.massSendText(sendMsg, Staff, function (err, result) { // 群发菜单给员工
+                api.massSendText(sendMsg, "100", function (err, result) { // 群发菜单给员工
         
                     console.log('err is:', err);
                     console.log('result is', result);
@@ -59,7 +58,7 @@ router.use('/', wechat(config, function (req, res, next) {
             }
         }
     
-
+    // cooker get Msg
     if(message.Content == "统计") {     // 获取统计后的信息 发送给煮饭阿姨
         var addUpOverStr = '';  
         addUpOverStr = send2Cooker();
@@ -67,7 +66,12 @@ router.use('/', wechat(config, function (req, res, next) {
         res.reply(addUpOverStr);
     }
 
+    // get staff select food
     addUp(message.Content,res);     // 菜品统计
+
+    if (message.Content == "哈哈") {
+        res.reply("嘻嘻");
+    }
 })); 
 
 function updateMenu() {     // 统计更新每日菜单
@@ -104,7 +108,7 @@ function addUp(content, res) {   // 发送菜单后，员工回复的统计
     
     var selectFood = 0;
     selectFood = parseInt(content);
-    if(selectFood > 0 && selectFood < foods.length) {   // 员工回复操作
+    if(selectFood > 0 && selectFood <= foods.length) {   // 员工回复操作
         foods[selectFood-1].numbers++;
         console.log(foods);
         res.reply("你已订餐完成");
@@ -131,6 +135,55 @@ function send2Cooker() {
     return sendCookerStr;
 }
 
+// 获取所有关注用户的openid
+function getUserOpenid() {
+    var UserNumbers = 0;
+    var UserOpenid = new Array();
+    api.getFollowers(function (err, result) {
+        // console.log(err);
+        console.log(result);
+        for (let i = 0; i < result.data.openid.length; i++) {
+
+            UserOpenid.push(result.data.openid[i]);
+            UserNumbers++;
+        }
+        // console.log(UserOpenid, UserNumbers);
+        addPerson2Group(UserOpenid);
+    });
+}
+
+/** 
+ * 每天饭点遍历关注人数，将新关注人员添加在分组中（排除煮饭阿姨）
+ * @param {function} 
+ */
+function addPerson2Group(openidUnifo) {
+    var openIdNow = openidUnifo;
+    var newAddPeople = [];
+    if (StaffNow.length < openIdNow.length) {
+        console.log("有新增成员");
+        // 遍历比较，找出新增加成员
+        openIdNow.forEach(element => {
+            if (StaffNow.indexOf(element) == -1) {
+                console.log('新增了' + element);
+                // 将新增成员添加进吃货分组
+                api.moveUserToGroup(element, 100, function (err, result) {
+                    console.log(err);
+                    console.log(result);
+                });
+                StaffNow.push(element);
+            }
+        });
+    } else if (StaffNow.length > openIdNow.length) {
+        // 如果成员相较之前减少
+        StaffNow = '';
+        StaffNow = [].concat(openIdNow);
+        console.log('减少之后的吃货数:', StaffNow);
+    } else {
+        console.log('没成员变化，不进行操作');
+        return;
+    }
+}
+
 /** 
  *  如到规定时间煮饭阿姨没主动查询统计好之后的值，则到时间主动推送给她
  *  
@@ -140,7 +193,7 @@ var send2CookerOnTIme = function () { // 固定时间发送统计之后的信息
     if (MenuGetFlag == 0) {
         var addUpOverStr = '';
         addUpOverStr = send2Cooker();
-        api.sendText('oZ1891Z9gSJfAjfu9Eu7kJdYEwA8', addUpOverStr, function (err, result) { // 发送客服消息
+        api.sendText(cookerId, addUpOverStr, function (err, result) { // 发送客服消息
             console.log(err);
             console.log(result);
         });
@@ -159,145 +212,11 @@ var send2CookerOnTIme = function () { // 固定时间发送统计之后的信息
     rule.minute = 30;
 
     schedule.scheduleJob(rule, function () {
-        console.log("每天6:30执行");
+        getUserOpenid();    // 更新吃货分组用户
+        console.log("每天5:30执行");
         obj();
         MenuGetFlag = 0; // 每天固定清除标识位
     });
 })(send2CookerOnTIme);
 
-// setTimePlan(send2CookerOnTIme);     // 每日阿姨没主动查询时的定时推送
-
-
-// 获取所有关注用户的openid
-function getUserOpenid() {
-    var UserNumbers = 0;
-    var UserOpenid = new Array();
-    api.getFollowers(function (err, result) {
-        console.log(err);
-        console.log(result);
-        for (let i = 0; i < result.data.openid.length; i++) {
-
-            UserOpenid.push(result.data.openid[i]);
-            UserNumbers++;
-        }
-        console.log(UserOpenid, UserNumbers);
-    });
-}
-
 module.exports = router;
-// api.massSendText("哈哈 狗屁", Staff, function (err, result) { // 群发菜单给员工
-
-//         console.log('err is:', err);
-//         console.log('result is', result);
-// });
-
-// getUserOpenid();
-
-        // api.sendText("oZ1891Z9gSJfAjfu9Eu7kJdYEwA8", sendMsg, function (err, result) { // 发送客服消息
-        //     console.log(err);
-        //     console.log(result);
-        // }); 
-
-// api.moveUserToGroup("oZ1891Z9gSJfAjfu9Eu7kJdYEwA8", 100, function (err, result) {
-//     console.log(err);
-//     console.log(result);
-// });
-
-// api.getGroups(function (err, result) { // 查询分组信息
-//     // for (let i = 0; i < result.groups.length; i++) {
-//     //     console.log(result.groups[i].name);
-//     // }
-//     console.log(result);
-//     console.log(err);
-// });
-
-
-
-// api.getGroups(function (err, result) {      // 查询分组信息
-//     for (let i = 0; i < result.groups.length; i++) {
-//         console.log(result.groups[i].name);
-//     }
-//     console.log(result);
-//     console.log(err);
-// });
-
-// api.getWhichGroup("oDfg30l59kYZ1Kx8PPa3ZfRReK6Q", function (err, result) {  // 查询openid用户在哪个分组
-//     console.log(result);
-//     console.log(err);
-// });
-
-// api.massSendText("群发测试hehe", Staff, function (result, err) {
-
-//     console.log(result);
-//     console.log(err);
-// });
-
-// api.massSendText("群发测试hehe", "oDfg30l59kYZ1Kx8PPa3ZfRReK6Q", function (result, err) {
-
-//     console.log(result);
-//     console.log(err);
-// });
-
-// api.deleteMass("1000000001", function (result, err) {
-//     console.log(result);
-//     console.log(err);
-// });
-
-// api.createGroup('员工分组', function (result, err) {    // id：100
-//     console.log(result);
-//     console.log(err);
-// });
-
-// api.createGroup('做饭阿姨', function (result, err) {    // id： 101
-//     console.log(result);
-//     console.log(err);
-// });
-
-// (function creatGroups() {
-//     api.getGroups(function (err, result) {      // 查询分组信息
-//         console.log(result);
-//         console.log(err);
-//         for (let i = 0; i < result.groups.length; i++) {
-//             if (result.groups[i].name != '员工分组') {
-//                 api.createGroup('员工分组', function (result, err) {    // id：100
-//                     console.log(result);
-//                     console.log(err);
-//                 });
-//             }
-//             else if(result.groups[i].name != '做饭阿姨') {
-//                 api.createGroup('做饭阿姨', function (result, err) {    // id： 101
-//                     console.log(result);
-//                     console.log(err);
-//                 });
-//             }
-//             console.log("分组已存在！");
-//         }
-//     });
-// })();
-
-// function moveUser2Groups() {
-
-// }
-
-// api.getFollowers(function (err, result) {
-//     console.log(err);
-//     console.log(result);
-// });
-
-// api.removeGroup(107, function (result, err) {
-
-//     console.log(result);
-//     console.log(err);
-// });
-
-
-// api.sendText('oDfg30pvv-0lPXBTORbyC0ZI8d7w', '哈哈哈 嘻嘻嘻嘻', function (err, result) {     // 发送客服消息
-//     console.log(err);
-//     console.log(result);
-// });
-
-// api.removeMenu(function (err, result) {      // 删除自定义菜单
-
-//     console.log(err);
-//     console.log(result);
-// });
